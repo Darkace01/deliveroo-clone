a26db5fc7862a0ebb41f8fb664625807369556d0
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-07-09 07:51:12 +0100

    tailwind set up with react navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text clasName='text-red-500'>Home Screen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
