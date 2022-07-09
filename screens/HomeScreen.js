21d9986cef30f77cd4a23d6f18cff15be2cf5505
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-07-09 10:06:48 +0100

    Category component and home header section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,58 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image, TextInput, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  ChevronDownIcon,
+  UserIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from 'react-native-heroicons/outline';
+import Categories from '../components/Categories';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  });
   return (
-    <View>
-      <Text className='text-red-500'>Home Screen</Text>
-    </View>
+    <SafeAreaView className='bg-white pt-5'>
+      <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <Image
+          source={{ uri: 'https://links.papareact.com/wru' }}
+          className='h-7 w-7 bg-grey-300 p-4 rounded-full'
+        />
+        <View className='flex-1'>
+          <Text className='font-bold text-gray-400 text-xs'>Deliver Now!</Text>
+          <Text className='font-bold text-xl'>
+            Current Location
+            <ChevronDownIcon size={20} color='#00CCBB' />
+          </Text>
+        </View>
+        <UserIcon size={35} color='#00CCBB' />
+      </View>
+      <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+        <View className='flex-row space-x-2 flex-1 bg-gray-200 p-3'>
+          <SearchIcon color='gray' size={20} />
+          <TextInput
+            placeholder='Restaurants and cuisinse'
+            keyboardType='default'
+          />
+        </View>
+        <AdjustmentsIcon color='#00CCBB' />
+      </View>
+      <ScrollView
+        className='bg-gray-100'
+        contentContainerStyle={{
+          paddingBottom: 10,
+        }}
+      >
+        <Categories />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
