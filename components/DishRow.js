43aa4af8873f42bdf186fcf3e3d7127eb47a4f05
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-07-16 12:01:05 +0100

    install currency formatter using yarn

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,5 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native';
+import Currency from 'react-currency-formatter';
 import React from 'react';
+import { urlFor } from '../sanity';
 
 const DishRow = ({ id, name, description, price, image }) => {
   return (
@@ -7,6 +9,15 @@ const DishRow = ({ id, name, description, price, image }) => {
       <View>
         <Text className='text-lg mb-1'>{name}</Text>
         <Text className='text-gray-400'>{description}</Text>
+        <Text className='text-gray-400 mt-2'>
+          <Currency quantity={price} currency='GBP' />
+        </Text>
+      </View>
+      <View>
+        {/* <Image
+          source={{ uri: urlFor(image).url() }}
+          className='h-20 w-20 bg-gray-300'
+        /> */}
       </View>
     </TouchableOpacity>
   );
